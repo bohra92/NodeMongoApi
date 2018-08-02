2e743006919798eb710e2b0024889d0beef2aa90
tanuj-mindtree <tanuj.bohra@mindtree.com>
2018-08-02 21:56:47 +0530

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,21 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodaApp', (err,db) => {
+  if (err) {
+    return console.log('Unable to connect');
+  }
+  console.log('Connected to server');
+  db.collection('User').insertOne({
+    age : '26',
+    completed: true
+  }, (err, result) => {
+    if (err) {
+      return console.log("error while inserting", err);
+    }
+    console.log(JSON.stringify(result.ops,undefined,2));
+
+  })
+
+  db.close();
+
+});
