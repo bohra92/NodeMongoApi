287480a5a67bddf444c26a5104fc61e467e52f22
Tanuj-mindtree <tanuj.bohra@mindtree.com>
2018-08-03 21:43:36 +0530

    added update file

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -3,19 +3,33 @@ const {
   ObjectID
 } = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodaApp', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/User', (err, db) => {
   if (err) {
     return console.log('Unable to connect');
   }
   console.log('Connected to server');
 
   //find one And update
-  db.collection('User').find().findOneAndUpdate({_id : new ObjectID("5b6328f043919282abbb0e57")}).then((docs) => {
-    console.log("TODOS:");
-    console.log(JSON.stringify(docs, undefined, 2));
+  db.collection('User').findOneAndUpdate({
+    _id: new ObjectID("5b647a3527cfec595e7eb52c")
+  }, {
+    $set: {
+      location: "Bengal"
+    },
+    $inc: {
+      age: 2
+    }
+
+  }, {
+    returnOriginal: false
+  }).then((results) => {
+
+    console.log(results);
   })
 
-//closing  db
-db.close();
+  //
+
+  //closing  db
+  db.close();
 
 });
