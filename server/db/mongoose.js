a052a0f0d69d345679965e4389c428d90a470d70
tanuj-mindtree <tanuj.bohra@mindtree.com>
2018-08-10 16:28:01 +0530

    added mongo URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://tanuj:M@1030501@ds119072.mlab.com:19072/offshore-data'|| 'mongodb://localhost:27017/TodoApp');
 mongoose.Promise = global.Promise;
 
 module.exports = {
